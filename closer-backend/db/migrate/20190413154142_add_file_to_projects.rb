45827a2c328bbfb9718eb1d4d60784f59115fb00
fahim-abdullah <fahimabdullahh@gmail.com>
2019-04-13 23:50:09 +0800

    attachment feature

---
+++ closer-backend/db/migrate/20190413154142_add_file_to_projects.rb
@@ -0,0 +1,5 @@
+class AddFileToProjects < ActiveRecord::Migration[5.2]
+  def change
+    add_column :projects, :file, :string
+  end
+end
--- closer-backend/db/schema.rb
+++ closer-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_153223) do
+ActiveRecord::Schema.define(version: 2019_04_13_154142) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_04_13_153223) do
     t.string "timeframe"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "file"
   end
 
 end
