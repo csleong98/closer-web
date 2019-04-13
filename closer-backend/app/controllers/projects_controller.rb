f72dffe934585d18b7a5e0890ca08760f6f0e7e4
fahim-abdullah <fahimabdullahh@gmail.com>
2019-04-14 07:49:41 +0800

    new x create

--- closer-backend/app/controllers/projects_controller.rb
+++ closer-backend/app/controllers/projects_controller.rb
@@ -2,23 +2,22 @@ class ProjectsController < ApplicationController
 	def new
 		@project = Project.new
 	end
-
-	def create 
+	def create
 		@project = Project.new(project_params)
 		if @project.save
-			redirect_to project_path
+			redirect_to projects_path
 		else
 			render 'new'
 		end
 	end
 
-	def show 
-		@project = Project.find(params[:id])
+	def index
+		@projects = Project.all
 	end
 
 	private
 	def project_params 
-		params.require(:project).permit(:title, :description, :file, :category, :timeframe)
+		params.require(:project).permit(:title, :description, :category, :file, :timeframe)
 	end
 
 end
